30f43e94305862ff098168ae14cb2037bdd4baf6
Ivan Nelson <ivan.sryvkin@bk.ru>
2024-07-20 10:03:01 +0300

    upd

---
+++ src/app/auth/email/page.tsx
@@ -0,0 +1,9 @@
+import Auth from '@/screens/auth/Auth'
+import type { NextPage } from 'next'
+
+const page: NextPage = () => {
+	return (
+			<Auth page='email'/>
+		)
+}
+export default page
---
+++ src/screens/auth/email/Email.tsx
@@ -0,0 +1,12 @@
+
+import { FC } from 'react'
+import styles from './Email.module.scss'
+
+const Email: FC = () => {
+	return (
+			 <div className={styles.wrapper}>
+					Email
+			 </div>
+		)
+}
+export default Email
